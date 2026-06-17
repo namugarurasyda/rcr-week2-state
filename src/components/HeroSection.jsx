1a71c7c78913aa4d2a14a1a105af2ec69b227ed6
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-17 22:45:59 +0300

    Editing the Herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,8 +8,7 @@ export default function Example() {
           <div className="max-w-xl lg:max-w-lg">
             <h2 className="text-4xl font-semibold tracking-tight text-white">Subscribe to our newsletter</h2>
             <p className="mt-4 text-lg text-gray-300">
-              Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
-              dolore.
+              Reach out to us for any inquiries or questions you may have. Our team is here to assist you and provide the information you need.
             </p>
             <div className="mt-6 flex max-w-md gap-x-4">
               <label htmlFor="email-address" className="sr-only">
