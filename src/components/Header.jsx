37fb0e9d77dc3479345312f692be01c5b8d903a4
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-19 23:55:17 +0300

    Changing the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ export default function Header() {
             <circle r={512} cx={512} cy={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
             <defs>
               <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                <stop stopColor="#7775D6" />
-                <stop offset={1} stopColor="#E935C1" />
+                <stop stopColor="#7584d6" />
+                <stop offset={1} stopColor="#5035e9" />
               </radialGradient>
             </defs>
           </svg>
