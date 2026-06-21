ae35ac1ef77d1af88c625f0ad1973458f0e53edd
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-21 23:33:18 +0300

    Import the fields

--- src/components/Comboobox.jsx
+++ src/components/Comboobox.jsx
@@ -1,17 +1,24 @@
+// Change your imports to look like this:
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Label, Field } from '@headlessui/react'
 
-
-function combobox({ currentUser, users }) {
+function Combobox({ currentUser, users }) {
   return (
     <Field>
       <Label>Assigned to</Label>
-      <Combobox name="user" options={users} displayValue={(user) => user?.name} defaultValue={currentUser}>
-        {(user) => (
-          <ComboboxOption value={user}>
-            <ComboboxLabel>{user.name}</ComboboxLabel>
-          </ComboboxOption>
-        )}
+      {/* Headless UI expects slightly different structures (Input + Options wrapper) */}
+      <Combobox defaultValue={currentUser}>
+        <ComboboxInput displayValue={(user) => user?.name} />
+        <ComboboxButton>▼</ComboboxButton>
+        <ComboboxOptions>
+          {users.map((user) => (
+            <ComboboxOption key={user.id} value={user}>
+              {user.name}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
       </Combobox>
     </Field>
   )
 }
-export default combobox;
+
+export default Combobox;
